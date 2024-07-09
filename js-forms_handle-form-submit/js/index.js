76385bcbd549732735cd06a2ebb244a8f7226903
Sercan Sebil <sercansebil95@googlemail.com>
2024-07-09 15:21:29 +0200

    complete task

---
+++ js-forms_handle-form-submit/js/index.js
@@ -0,0 +1,30 @@
+console.clear();
+// Use JavaScript to react to the form submission.
+
+// Log all form data (in object form) into the console in the submit event handler.
+
+// Make sure the form is reset and the First Name input is focused after submission.
+
+const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+  event.target.reset();
+
+  const age = parseInt(data.age);
+  const badness = parseInt(data.badness);
+  const firstName = data.firstName;
+  const ageBadnessSum = age + badness;
+  console.log(`The age-badness-sum of ${firstName} is ${ageBadnessSum}`);
+});
+
+// ## Bonus: Calculate and log the age-badness-sum 🤷‍♀️
+
+// The formula is: `age + badness`
+
+// Log to the console _(replace the values between the quotes)_:
+
+// > The age-badness-sum of "firstName" is "age-badness-sum"

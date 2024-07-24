e355de82ee767b5ca3380b4e80eaeef017d8dcda
Sercan Sebil <sercansebil95@googlemail.com>
2024-07-24 17:37:47 +0200

    complete task

---
+++ react/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+export default function PokemonList() {
+  const [pokemon, setPokemon] = useState([]);
+  const [page, setPage] = useState(0);
+
+  function handlePreviousPage() {
+    setPage(page - 1);
+  }
+  function handleNextPage() {
+    setPage(page + 1);
+  }
+
+  useEffect(() => {
+    async function loadPokemon() {
+      try {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon?offset=${page}`
+        );
+        const data = await response.json();
+        setPokemon(data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    loadPokemon();
+  }, [page]);
+
+  return (
+    <main>
+      {page >= 1 ? (
+        <button onClick={handlePreviousPage} type="button">
+          Previous Page
+        </button>
+      ) : null}
+      <button type="button" onClick={handleNextPage}>
+        Next Page
+      </button>
+      <ul>
+        {pokemon.map(({ name }) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+    </main>
+  );
+}

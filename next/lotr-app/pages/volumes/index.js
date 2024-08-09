3972f4378ad73d37e67ebdda60b66ab43a49bb80
Sercan Sebil <sercansebil95@googlemail.com>
2024-08-09 15:50:26 +0200

    implement random volume button

--- next/lotr-app/pages/volumes/index.js
+++ next/lotr-app/pages/volumes/index.js
@@ -5,11 +5,26 @@ import { useRouter } from "next/router";
 
 export default function MoviesList() {
   const router = useRouter();
+  function getRandomVolume() {
+    return volumes[Math.floor(Math.random() * volumes.length)];
+  }
+
   return (
     <>
       <h1>Lord Of The Rings</h1>
       <p>{introduction}</p>
       <h2>All Volumes</h2>
+      <button
+        onClick={() => {
+          const randomVolume = getRandomVolume();
+          if (confirm("You want to go to a random volume?")) {
+            router.push(`/volumes/${randomVolume.slug}`);
+          }
+        }}
+      >
+        Go to a Random Volume
+      </button>
+
       <ul>
         {volumes.map((volume) => (
           <li key={volume.slug}>
